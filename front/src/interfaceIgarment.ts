655eced42d044012bbf25651170746b215008a1e
Laura <lorauhhuhh@hotmail.com>
2019-08-02 17:59:17 +0200

    haciendo el front de garments

---
+++ front/src/interfaceIgarment.ts
@@ -0,0 +1,11 @@
+export interface IGarment {
+    _id: string;
+    reference: string;
+    description?: string;
+    season: string;
+    sizes: [string],
+    colors: [string];
+    users: [string];
+    images?: [string];
+
+}
