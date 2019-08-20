9e21eb2c6ed60965f7bd088b030e339373053890
unknown <you@example.com>
2019-08-20 14:34:33 +0200

    metiendo imagenes y carousel

--- front/src/components/LayoutPage.tsx
+++ front/src/components/LayoutPage.tsx
@@ -1,220 +1,224 @@
-import React, { useEffect } from "react";
-import Navbar from "./Navbar";
-import { Switch, Route, RouteComponentProps } from "react-router";
-import ShowUsers from "./ShowUsers";
-import EditUser from "./EditUser";
-import AddUser from "./AddUser";
-import { IRole } from "../interfaceRole";
-import { IGlobalState } from "../reducers/reducers";
-import * as userActions from "../actions/userActions";
-import { connect } from "react-redux";
-import ShowGarments from "./ShowGarments";
-import AddGarment from "./AddGarment";
-import { ISize } from "../interfaceSize";
-import { IColor } from "../interfaceColor";
-import { IUser } from "../interfaceIuser";
-import * as garmentActions from "../actions/garmentActions";
-import EditGarment from "./EditGarment";
-import { ISeason } from "../interfaceSeason";
-import Search from "./Search";
-import { IGarment } from "../interfaceIgarment";
+import React, { useEffect } from 'react';
+import Navbar from './Navbar';
+import { Switch, Route, RouteComponentProps } from 'react-router';
+import ShowUsers from './ShowUsers';
+import EditUser from './EditUser';
+import AddUser from './AddUser';
+import { IRole } from '../interfaceRole';
+import { IGlobalState } from '../reducers/reducers';
+import * as userActions from '../actions/userActions';
+import { connect } from 'react-redux';
+import ShowGarments from './ShowGarments';
+import AddGarment from './AddGarment';
+import { ISize } from '../interfaceSize';
+import { IColor } from '../interfaceColor';
+import { IUser } from '../interfaceIuser';
+import * as garmentActions from '../actions/garmentActions';
+import EditGarment from './EditGarment';
+import { ISeason } from '../interfaceSeason';
+import Search from './Search';
+import { IGarment } from '../interfaceIgarment';
+import "./styles/LayoutPage.css";
 
 interface IPropsGlobal {
-  token: string;
-  roles: IRole[];
-  users: IUser[];
-  sizes: ISize[];
-  colors: IColor[];
-  seasons: ISeason[];
-  garments: IGarment[];
-  setUsers: (users: IUser[]) => void;
-  setRoles: (roles: []) => void;
-  setSizes: (sizes: ISize[]) => void;
-  setColors: (colors: IColor[]) => void;
-  setSeasons: (seasons: ISeason[]) => void;
-  setGarments: (garments: []) => void;
+	token: string;
+	roles: IRole[];
+	users: IUser[];
+	sizes: ISize[];
+	colors: IColor[];
+	seasons: ISeason[];
+	garments: IGarment[];
+	setUsers: (users: IUser[]) => void;
+	setRoles: (roles: []) => void;
+	setSizes: (sizes: ISize[]) => void;
+	setColors: (colors: IColor[]) => void;
+	setSeasons: (seasons: ISeason[]) => void;
+	setGarments: (garments: []) => void;
 }
 
-const LayoutPage: React.FC<IPropsGlobal & RouteComponentProps> = props => {
-  const getRoles = () => {
-    if (props.token) {
-      fetch("http://localhost:3000/api/roles/list", {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: "Bearer " + props.token
-        }
-      }).then(response => {
-        if (response.ok) {
-          response.json().then(roles => {
-            props.setRoles(roles);
-          });
-        }
-      });
-    }
-  };
+const LayoutPage: React.FC<IPropsGlobal & RouteComponentProps> = (props) => {
+	const getRoles = () => {
+		if (props.token) {
+			fetch('http://localhost:3000/api/roles/list', {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: 'Bearer ' + props.token
+				}
+			}).then((response) => {
+				if (response.ok) {
+					response.json().then((roles) => {
+						props.setRoles(roles);
+					});
+				}
+			});
+		}
+	};
 
-  const getSizes = () => {
-    if (props.token) {
-      fetch("http://localhost:3000/api/sizes/list", {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: "Bearer " + props.token
-        }
-      }).then(response => {
-        if (response.ok) {
-          response.json().then(sizes => {
-            props.setSizes(sizes);
-          });
-        }
-      });
-    }
-  };
+	const getSizes = () => {
+		if (props.token) {
+			fetch('http://localhost:3000/api/sizes/list', {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: 'Bearer ' + props.token
+				}
+			}).then((response) => {
+				if (response.ok) {
+					response.json().then((sizes) => {
+						props.setSizes(sizes);
+					});
+				}
+			});
+		}
+	};
 
-  const getColors = () => {
-    if (props.token) {
-      fetch("http://localhost:3000/api/colors/list", {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: "Bearer " + props.token
-        }
-      }).then(response => {
-        if (response.ok) {
-          response.json().then(colors => {
-            props.setColors(colors);
-          });
-        }
-      });
-    }
-  };
+	const getColors = () => {
+		if (props.token) {
+			fetch('http://localhost:3000/api/colors/list', {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: 'Bearer ' + props.token
+				}
+			}).then((response) => {
+				if (response.ok) {
+					response.json().then((colors) => {
+						props.setColors(colors);
+					});
+				}
+			});
+		}
+	};
 
-  const getUsers = () => {
-    if (props.token) {
-      fetch("http://localhost:3000/api/users/list", {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: "Bearer " + props.token
-        }
-      }).then(response => {
-        if (response.ok) {
-          response.json().then(users => {
-            props.setUsers(users);
-          });
-        }
-      });
-    }
-  };
+	const getUsers = () => {
+		if (props.token) {
+			fetch('http://localhost:3000/api/users/list', {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: 'Bearer ' + props.token
+				}
+			}).then((response) => {
+				if (response.ok) {
+					response.json().then((users) => {
+						props.setUsers(users);
+					});
+				}
+			});
+		}
+	};
 
-  const getSeasons = () => {
-    if (props.token) {
-      fetch("http://localhost:3000/api/seasons/list", {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: "Bearer " + props.token
-        }
-      }).then(response => {
-        if (response.ok) {
-          response.json().then(seasons => {
-            props.setSeasons(seasons);
-          });
-        }
-      });
-    }
-  };
+	const getSeasons = () => {
+		if (props.token) {
+			fetch('http://localhost:3000/api/seasons/list', {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: 'Bearer ' + props.token
+				}
+			}).then((response) => {
+				if (response.ok) {
+					response.json().then((seasons) => {
+						props.setSeasons(seasons);
+					});
+				}
+			});
+		}
+	};
 
-  const getGarments = () => {
-    if (props.token) {
-      fetch("http://localhost:3000/api/garments/list", {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: "Bearer " + props.token
-        }
-      }).then(response => {
-        if (response.ok) {
-          response.json().then(garments => {
-            props.setGarments(garments);
-          });
-        }
-      });
-    }
-  };
+	const getGarments = () => {
+		if (props.token) {
+			fetch('http://localhost:3000/api/garments/list', {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: 'Bearer ' + props.token
+				}
+			}).then((response) => {
+				if (response.ok) {
+					response.json().then((garments) => {
+						props.setGarments(garments);
+					});
+				}
+			});
+		}
+	};
 
-  useEffect(() => {
-    getRoles();
-  }, []);
+	useEffect(() => {
+		getRoles();
+	}, []);
 
-  useEffect(() => {
-    getSizes();
-  }, []);
+	useEffect(() => {
+		getSizes();
+	}, []);
 
-  useEffect(() => {
-    getColors();
-  }, []);
+	useEffect(() => {
+		getColors();
+	}, []);
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+	useEffect(() => {
+		getUsers();
+	}, []);
 
-  useEffect(() => {
-    getSeasons();
-  }, []);
+	useEffect(() => {
+		getSeasons();
+	}, []);
 
-  useEffect(() => {
-    getGarments();
-  }, []);
+	useEffect(() => {
+		getGarments();
+	}, []);
 
-  return (
-    <div>
-      <Navbar />
-      <Switch>
-        <Route path="/users/add" exact component={AddUser} />
-        <Route path="/users/" exact component={ShowUsers} />
-        {/* <Route path="/users/edit/:user_id" exact component={EditUser} /> */}
-        <Route path="/users/:user_id" exact component={EditUser} />
-        <Route
-          path="/seasons/:season_id/garments/add"
-          exact
-          component={AddGarment}
-        />
-        <Route
-          path="/seasons/:season_id/garments/"
-          exact
-          component={ShowGarments}
-        />
-        <Route
-          path="/seasons/:season_id/garments/edit/:garment_id"
-          exact
-          component={EditGarment}
-        />
-        <Route
-          path="/search"
-          exact
-          component={Search}
-        />
-      </Switch>
-    </div>
-  );
+	return (
+		// <div className="carouselLayout">
+		// 	<div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+		// 		<div className="carousel-inner">
+		// 			<div className="carousel-item active">
+		// 				<img className="d-block w-80" src="/images/Interfaz Layout1.png" alt="First slide" />
+		// 			</div>
+		// 			<div className="carousel-item">
+		// 				<img className="d-block w-80" src="/images/Interfaz Layout2.png" alt="Second slide" />
+		// 			</div>
+		// 		</div>
+		// 		<a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		// 			<span className="carousel-control-prev-icon" aria-hidden="true" />
+		// 			<span className="sr-only">Previous</span>
+		// 		</a>
+		// 		<a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		// 			<span className="carousel-control-next-icon" aria-hidden="true" />
+		// 			<span className="sr-only">Next</span>
+		// 		</a>
+		// 	</div>
+    //   </div>
+
+			<div>
+				<Navbar />
+				<Switch>
+					<Route path="/users/add" exact component={AddUser} />
+					<Route path="/users/" exact component={ShowUsers} />
+					{/* <Route path="/users/edit/:user_id" exact component={EditUser} /> */}
+					<Route path="/users/:user_id" exact component={EditUser} />
+					<Route path="/seasons/:season_id/garments/add" exact component={AddGarment} />
+					<Route path="/seasons/:season_id/garments/" exact component={ShowGarments} />
+					<Route path="/seasons/:season_id/garments/edit/:garment_id" exact component={EditGarment} />
+					<Route path="/search" exact component={Search} />
+				</Switch>
+			</div>
+		
+	);
 };
 
 const mapStateToProps = (state: IGlobalState) => ({
-  token: state.token,
-  roles: state.roles,
-  sizes: state.sizes,
-  colors: state.colors,
-  users: state.users,
-  seasons: state.seasons,
-  garments: state.garments
+	token: state.token,
+	roles: state.roles,
+	sizes: state.sizes,
+	colors: state.colors,
+	users: state.users,
+	seasons: state.seasons,
+	garments: state.garments
 });
 
 const mapDispatchToProps = {
-  setRoles: userActions.setRoles,
-  setUsers: userActions.setUsers,
-  setSizes: garmentActions.setSizes,
-  setColors: garmentActions.setColors,
-  setSeasons: garmentActions.setSeasons,
-  setGarments: garmentActions.setGarments
+	setRoles: userActions.setRoles,
+	setUsers: userActions.setUsers,
+	setSizes: garmentActions.setSizes,
+	setColors: garmentActions.setColors,
+	setSeasons: garmentActions.setSeasons,
+	setGarments: garmentActions.setGarments
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LayoutPage);
+export default connect(mapStateToProps, mapDispatchToProps)(LayoutPage);
--- front/src/components/LoginPage.tsx
+++ front/src/components/LoginPage.tsx
@@ -1,255 +1,245 @@
-import React from "react";
-import * as actions from "../actions/userActions";
-import { connect } from "react-redux";
-import jwt from "jsonwebtoken";
-import { IMyUser } from "../reducers/myUserReducer";
-import "./styles/LoginPage.css";
+import React from 'react';
+import * as actions from '../actions/userActions';
+import { connect } from 'react-redux';
+import jwt from 'jsonwebtoken';
+import { IMyUser } from '../reducers/myUserReducer';
+import './styles/LoginPage.css';
 
 interface IProps {
-  saveToken: (token: string) => void;
-  saveMyUser: (myUser: IMyUser) => void;
+	saveToken: (token: string) => void;
+	saveMyUser: (myUser: IMyUser) => void;
 }
 
-const Login: React.FC<IProps> = props => {
-  const [email, setEmail] = React.useState<string>("");
-  const [password, setPassword] = React.useState<string>("");
-  const [error, setError] = React.useState("");
+const Login: React.FC<IProps> = (props) => {
+	const [ email, setEmail ] = React.useState<string>('');
+	const [ password, setPassword ] = React.useState<string>('');
+	const [ error, setError ] = React.useState('');
 
-  const updateEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.currentTarget.value);
-    setError("");
-  };
+	const updateEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setEmail(event.currentTarget.value);
+		setError('');
+	};
 
-  const updatePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.currentTarget.value);
-    setError("");
-  };
+	const updatePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.currentTarget.value);
+		setError('');
+	};
 
-  const getToken = () => {
-    if (email && password) {
-      fetch("http://localhost:3000/api/auth", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password
-        })
-      }).then(res => {
-        if (res.ok) {
-          res.text().then(token => {
-            localStorage.setItem("token", token);
-            props.saveToken(token);
-            const decode = jwt.decode(token);
-            console.log(decode);
-            if (typeof decode !== "string" && decode !== null) {
-              props.saveMyUser(decode);
-            }
-          });
-        }
-      });
-    } else {
-      setError("you need to introduce you email and password");
-    }
-  };
+	const getToken = () => {
+		if (email && password) {
+			fetch('http://localhost:3000/api/auth', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					email: email,
+					password: password
+				})
+			}).then((res) => {
+				if (res.ok) {
+					res.text().then((token) => {
+						localStorage.setItem('token', token);
+						props.saveToken(token);
+						const decode = jwt.decode(token);
+						console.log(decode);
+						if (typeof decode !== 'string' && decode !== null) {
+							props.saveMyUser(decode);
+						}
+					});
+				}
+			});
+		} else {
+			setError('you need to introduce you email and password');
+		}
+	};
 
-  return (
-    //   <div className="container">
-    //   <div className="row justify-content-center">
-    //     <div className="col-6">
-    //       <div className="form-group" id="login">
-    //         <h3>Login</h3>
-    //         <br />
-    //         <h4>Email</h4>
-    //         <input
-    //           type="text"
-    //           id="username"
-    //           placeholder=""
-    //           className="form-control"
-    //           value={email}
-    //           onChange={updateEmail}
-    //         />
-    //         <br />
-    //         <h4>Password</h4>
-    //         <input
-    //           type="password"
-    //           id="password"
-    //           placeholder=""
-    //           className="form-control"
-    //           value={password}
-    //           onChange={updatePassword}
-    //         />
-    //         <br />
-    //         <button
-    //           type="submit"
-    //           className="btn btn-outline-info"
-    //           onClick={getToken}
-    //         >
-    //           Sign in
-    //         </button>
-    //         {error && <div className="div">{error}</div>}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+	return (
+		//   <div className="container">
+		//   <div className="row justify-content-center">
+		//     <div className="col-6">
+		//       <div className="form-group" id="login">
+		//         <h3>Login</h3>
+		//         <br />
+		//         <h4>Email</h4>
+		//         <input
+		//           type="text"
+		//           id="username"
+		//           placeholder=""
+		//           className="form-control"
+		//           value={email}
+		//           onChange={updateEmail}
+		//         />
+		//         <br />
+		//         <h4>Password</h4>
+		//         <input
+		//           type="password"
+		//           id="password"
+		//           placeholder=""
+		//           className="form-control"
+		//           value={password}
+		//           onChange={updatePassword}
+		//         />
+		//         <br />
+		//         <button
+		//           type="submit"
+		//           className="btn btn-outline-info"
+		//           onClick={getToken}
+		//         >
+		//           Sign in
+		//         </button>
+		//         {error && <div className="div">{error}</div>}
+		//       </div>
+		//     </div>
+		//   </div>
+		// </div>
 
-    // <div className="App">
-    //   <div className="d-flex justify-content-center">
-    //     ​
-    //     <div className="col-md-6 col-lg-5">
-    //       ​
-    //       <div className="card">
-    //         ​
-    //         <div className="card-body mx-4">
-    //           ​
-    //           <div className="text-center">
-    //             <h3 className="dark-grey-text mb-5">
-    //               <strong>Sign in</strong>
-    //             </h3>
-    //           </div>
-    //           ​
-    //           <div className="md-form">
-    //             <input
-    //             type="text"
-    //             id="Form-email1"
-    //             className="form-control"
-    //             value={email}
-    //             onChange={updateEmail}  />
-    //             <label>Your email</label>
-    //           </div>
-    //           ​
-    //           <div className="md-form pb-3">
-    //             <input
-    //               type="password"
-    //               id="Form-pass1"
-    //               className="form-control"
-    //               value={password}
-    //               onChange={updatePassword}
-    //             />
-    //             <label>Your password</label>
-    //             <p className="font-small blue-text d-flex justify-content-end">
-    //               Forgot{" "}
-    //               <a href="#" className="blue-text ml-1">
-    //                 Password?
-    //               </a>
-    //             </p>
-    //           </div>
-    //           ​
-    //           <div className="text-center mb-3">
-    //             <button
-    //               type="submit"
-    //               className="btn blue-gradient btn-block btn-rounded z-depth-1a waves-effect waves-light"
-    //               onClick={getToken}
-    //             >
-    //               Sign in
-    //             </button>
-    //             {error && <div className="div">{error}</div>}
-    //           </div>
-    //           ​ ​
-    //         </div>
-    //         ​
-    //         <div className="modal-footer mx-5 pt-3 mb-1">
-    //           <p className="font-small grey-text d-flex justify-content-end">
-    //             Not a member?{" "}
-    //             <a href="#" className="blue-text ml-1">
-    //               Sign Up
-    //             </a>
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="container login">
-    
-      <div className="d-flex justify-content-center">
-        ​
-        <div className="col-md-6 col-lg-5">
-          ​
-          <div className="card-login">
-            ​
-            <div className="card-body-login mx-4">
-              ​
-              <img
-                className="logo"
-                src="images/LogoLabtex.png"
-                alt=""
-                width="90"
-                height="58"
-              />
-              <div className="text-center">
-                <h3 className="dark-grey-text mb-5">
-                  {/* <strong>Sign in</strong> */}
-                </h3>
-              </div>
-              ​
-              <div className="md-form">
-                <input
-                  type="text"
-                  id="Form-email1"
-                  className="form-control"
-                  value={email}
-                  onChange={updateEmail}
-                />
-                <label className="label1" htmlFor="Form-email1">
-                  Your email
-                </label>
-              </div>
-              ​
-              <div className="md-form pb-3">
-                <input
-                  type="password"
-                  id="Form-pass1"
-                  className="form-control"
-                  value={password}
-                  onChange={updatePassword}
-                />
-                <label className="label2" htmlFor="Form-pass1">
-                  Your password
-                </label>
-                <p className="font-small blue-text d-flex justify-content-end" id="forgot">
-                  Forgot{" "}
-                  <a href="#" className="blue-text ml-1" id="azul1">
-                    Password?
-                  </a>
-                </p>
-              </div>
-              ​
-              <div className="text-center mb-3">
-                <button
-                  type="submit"
-                  className="btn blue-gradient btn-block btn-rounded z-depth-1a waves-effect waves-light"
-                  onClick={getToken}
-                >
-                  Sign in
-                </button>
-                {error && <div className="div">{error}</div>}
-              </div>
-              ​ ​
-            </div>
-            ​
-            <div className="modal-footer mx-5 pt-3 mb-1">
-              <p className="font-small grey-text d-flex justify-content-end" id="member">
-                Not a member?{" "}
-                <a href="#" className="blue-text ml-1" id="azul2">
-                  Sign Up
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+		// <div className="App">
+		//   <div className="d-flex justify-content-center">
+		//     ​
+		//     <div className="col-md-6 col-lg-5">
+		//       ​
+		//       <div className="card">
+		//         ​
+		//         <div className="card-body mx-4">
+		//           ​
+		//           <div className="text-center">
+		//             <h3 className="dark-grey-text mb-5">
+		//               <strong>Sign in</strong>
+		//             </h3>
+		//           </div>
+		//           ​
+		//           <div className="md-form">
+		//             <input
+		//             type="text"
+		//             id="Form-email1"
+		//             className="form-control"
+		//             value={email}
+		//             onChange={updateEmail}  />
+		//             <label>Your email</label>
+		//           </div>
+		//           ​
+		//           <div className="md-form pb-3">
+		//             <input
+		//               type="password"
+		//               id="Form-pass1"
+		//               className="form-control"
+		//               value={password}
+		//               onChange={updatePassword}
+		//             />
+		//             <label>Your password</label>
+		//             <p className="font-small blue-text d-flex justify-content-end">
+		//               Forgot{" "}
+		//               <a href="#" className="blue-text ml-1">
+		//                 Password?
+		//               </a>
+		//             </p>
+		//           </div>
+		//           ​
+		//           <div className="text-center mb-3">
+		//             <button
+		//               type="submit"
+		//               className="btn blue-gradient btn-block btn-rounded z-depth-1a waves-effect waves-light"
+		//               onClick={getToken}
+		//             >
+		//               Sign in
+		//             </button>
+		//             {error && <div className="div">{error}</div>}
+		//           </div>
+		//           ​ ​
+		//         </div>
+		//         ​
+		//         <div className="modal-footer mx-5 pt-3 mb-1">
+		//           <p className="font-small grey-text d-flex justify-content-end">
+		//             Not a member?{" "}
+		//             <a href="#" className="blue-text ml-1">
+		//               Sign Up
+		//             </a>
+		//           </p>
+		//         </div>
+		//       </div>
+		//     </div>
+		//   </div>
+		// </div>
+		<div className="BackgroundImageLogin">
+			<div className="container">
+				<div className="d-flex justify-content-center">
+					​
+					<div className="col-md-6 col-lg-5">
+						​
+						<div className="card-login">
+							​
+							<div className="card-body-login mx-4">
+								​
+								<img className="logo" src="images/LogoLabtex.png" alt="" width="90" height="58" />
+								<div className="text-center">
+									<h3 className="dark-grey-text mb-5">{/* <strong>Sign in</strong> */}</h3>
+								</div>
+								​
+								<div className="md-form">
+									<input
+										type="text"
+										id="Form-email1"
+										className="form-control"
+										value={email}
+										onChange={updateEmail}
+									/>
+									<label className="label1" htmlFor="Form-email1">
+										Your email
+									</label>
+								</div>
+								​
+								<div className="md-form pb-3">
+									<input
+										type="password"
+										id="Form-pass1"
+										className="form-control"
+										value={password}
+										onChange={updatePassword}
+									/>
+									<label className="label2" htmlFor="Form-pass1">
+										Your password
+									</label>
+									<p className="font-small blue-text d-flex justify-content-end" id="forgot">
+										Forgot{' '}
+										<a href="#" className="blue-text ml-1" id="azul1">
+											Password?
+										</a>
+									</p>
+								</div>
+								​
+								<div className="text-center mb-3">
+									<button
+										type="submit"
+										className="btn blue-gradient btn-block btn-rounded z-depth-1a waves-effect waves-light"
+										onClick={getToken}
+									>
+										Sign in
+									</button>
+									{error && <div className="div">{error}</div>}
+								</div>
+								​ ​
+							</div>
+							​
+							<div className="modal-footer mx-5 pt-3 mb-1">
+								<p className="font-small grey-text d-flex justify-content-end" id="member">
+									Not a member?{' '}
+									<a href="#" className="blue-text ml-1" id="azul2">
+										Sign Up
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const mapDispatchToProps = {
-  saveToken: actions.saveToken,
-  saveMyUser: actions.saveMyUser
+	saveToken: actions.saveToken,
+	saveMyUser: actions.saveMyUser
 };
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(Login);
+export default connect(null, mapDispatchToProps)(Login);
