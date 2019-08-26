2dc67aa1a367bbdd5bb790d2318d124c2983d019
unknown <you@example.com>
2019-08-26 14:43:48 +0200

    con carousel

--- front/src/components/LayoutBackground.tsx
+++ front/src/components/LayoutBackground.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
 function LayoutBackground() {
 	return (
-		<div className="container-fluid">
-			<div className="BackgroundImageLayout" />
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<div className="BackgroundImageLayout">
+						<div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+							<div className="carousel-inner">
+								<div className="carousel-item active">
+									<img className="d-block w-100 " src="/images/Interfaz Layout2Peq.png" alt="First slide" />
+								</div>
+								<div className="carousel-item">
+									<img className="d-block w-100" src="/images/Interfaz Layout4Peq.png" alt="Second slide" />
+								</div>
+								{/* <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Third slide"/>
+    </div> */}
+							</div>
+							<a
+								className="carousel-control-prev"
+								href="#carouselExampleControls"
+								role="button"
+								data-slide="prev"
+							>
+								<span className="carousel-control-prev-icon" aria-hidden="true" />
+								<span className="sr-only">Previous</span>
+							</a>
+							<a
+								className="carousel-control-next"
+								href="#carouselExampleControls"
+								role="button"
+								data-slide="next"
+							>
+								<span className="carousel-control-next-icon" aria-hidden="true" />
+								<span className="sr-only">Next</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
